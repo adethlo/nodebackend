efd16382edd8ae55fa0e8c2ff7a1ef2fbf47affc
Alexander Dethloff <alexander.dethloff.89@gmail.com>
2018-06-18 22:40:00 +0000

    initial

---
+++ warbler-server/index.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+
+const express       = require("express"),
+      app           = express(),
+      cors          = require("cors"),
+      bodyParser    = require("body-parser"),
+      db            = require("./models"),
+      errorHandler  = require("./handlers/error");
+      
+const authRoutes        = require("./routes/auth"),
+      messagesRoutes    = require("./routes/messages");
+
+const {loginRequired, ensureCorrectUser} = require("./middleware/auth");
+      
+const PORT = 8081;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// all my routes
+app.use("api/auth", authRoutes);
+app.use(
+    "api/users/:id/messages", 
+    loginRequired, 
+    ensureCorrectUser, 
+    messagesRoutes
+);
+
+app.get("/api/messages", loginRequired, async function(req, res, next){
+    try {
+        let messages = await db.Message.find()
+            .sort({createdAt: "desc"})
+            .populate("user", {
+                username: true,
+                profileImageUrl: true
+            });
+        return res.status(200).json(messages);
+    } catch(err) {
+        return next(err);
+    }
+});
+
+
+app.use(function(req, res, next) {
+    let err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, function() {
+    console.log(`Server is starting on port ${PORT}`);
+});
